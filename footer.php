c9bac404f5dec0c419d800d79f46c9f91d4d11da
Martin Kihuha <mkihuha@gmail.com>
2019-09-13 13:29:08 +0300

    Styled Footer

--- footer.php
+++ footer.php
@@ -13,14 +13,24 @@
 
         </div><!-- #content -->
 
-        <footer id="colophon" class="site-footer">
-            <div class="site-info">
-                <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cubeloftmd' ) ); ?>">
+        <footer id="colophon" class="site-footer mdl-mini-footer">
+            <div class="site-info mdl-mini-footer__left-section">
+                <div class="mdl-logo">
                     <?php
-                    /* translators: %s: CMS name, i.e. WordPress. */
-                    printf( esc_html__( 'Proudly powered by %s', 'cubeloftmd' ), 'WordPress' );
+                    /* translators: 1: Theme name, 2: Theme author. */
+                    printf( esc_html__( 'Theme: %1$s by %2$s.', 'cubeloftmd' ), 'cubeloftmd', '<a href="http://www.wakilichapchap.co.ke">Martin Kihuha</a>' );
                     ?>
-                </a>
+                </div>
+                <ul class="mdl-mini-footer__link-list">
+                    <li>
+                        <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cubeloftmd' ) ); ?>">
+                            <?php
+                            /* translators: %s: CMS name, i.e. WordPress. */
+                            printf( esc_html__( 'Proudly powered by %s', 'cubeloftmd' ), 'WordPress' );
+                            ?>
+                        </a>
+                    </li>
+                </ul>
                 <span class="sep"> | </span>
                     <?php
                     /* translators: 1: Theme name, 2: Theme author. */
